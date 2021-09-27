630ed59d19253fca06344c60cb16957f53479423
johndanny98 <dannydev456@gmail.com>
2021-09-27 14:00:03 +0530

    navbar completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React,{Component} from "react"
-import {Container,Navbar,Nav} from 'react-bootstrap'
+import { Container,Navbar,Nav,NavDropdown} from 'react-bootstrap'
 
 class Header extends Component{
     render(){
@@ -9,22 +9,22 @@ class Header extends Component{
             paddingRight:"20px"
         }
         return(
-            <Navbar sticky = "top" expand = "sm" variant = "dark" bg = "dark">
-                 <Container>
-                    <Navbar.Brand href = "#intro" style = {{fontSize: "20px"}}>John Daniel</Navbar.Brand>
-                    <Navbar.Toggle aria-controls = "responsive-navbar-nav" />
-                    <Navbar.Collapse className = "justify-content-end">
-                        <Nav className = "mr-md-2">
-                            <Nav.Link href = "#about" style = {styles}> About </Nav.Link>
-                            <Nav.Link href = "#experience" style = {styles}> Experience </Nav.Link>
-                            <Nav.Link href = "#skills" style = {styles}> Skills </Nav.Link>
-                            <Nav.Link href = "#projects" style = {styles}> Projects </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                
-                    </Container>
-            </Navbar>
-
+          <Navbar sticky = "top" expand = "sm" variant = "dark" bg = "dark">
+          <Container>
+              <Navbar.Brand href = "#intro" style = {{fontSize: "25px"}}> 
+                  John Daniel
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls = "responsive-navbar-nav" />
+              <Navbar.Collapse className = "justify-content-end">
+                  <Nav className = "mr-md-2">
+                      <Nav.Link href = "#about" style = {styles}> About </Nav.Link>
+                      <Nav.Link href = "#experience" style = {styles}> Experience </Nav.Link>
+                      <Nav.Link href = "#skills" style = {styles}> Skills </Nav.Link>
+                      <Nav.Link href = "#projects" style = {styles}> Projects </Nav.Link>
+                  </Nav>
+              </Navbar.Collapse>
+          </Container>
+      </Navbar>
         );
     }
 }
